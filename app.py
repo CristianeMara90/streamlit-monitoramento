76fb32abf7c034b0b4c97a0b123a8f95aef30612
CristianeMara90 <167999613+CristianeMara90@users.noreply.github.com>
2026-01-29 15:44:03 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Monitoramento de Fluxo")
+
+st.title("📊 Monitoramento de Fluxo")
+st.write("Aplicativo publicado com sucesso no Streamlit 🚀")
+
+st.success("Tudo funcionando!")
